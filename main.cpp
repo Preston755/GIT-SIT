c23cd28ff51ba1ab2ed5be5c25ea94dd36296fe5
Preston755 <danilpopov226@gmail.com>
2026-05-24 20:26:02 +0400

    добавлена функция fib(n)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+#include <iostream> 
 using namespace std;
 
+int f(int n){
+    if (n == 1 || n == 2){
+        return 1;
+    }
+    return f(n - 1) + f(n - 2);
+}
+
 int main(){
-    cout << "Hello, World" << endl;
+    int n;
+    cout << "Введите номер числа Фибоначчи: ";
+    cin >> n;
+    
+    int r = f(n);
+    cout << "fib(" << n << ") = " << r << endl;
+
     return 0;
 }
