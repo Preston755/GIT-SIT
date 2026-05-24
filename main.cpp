177c00a6d5a0c05cc5c7f44059da5e5097d32901
Preston755 <danilpopov226@gmail.com>
2026-05-24 20:27:24 +0400

    исправленная программа 'fib(10) = 34'

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int f(int n){
-    if (n == 1 || n == 2){
+    if (n == 1){
+        return 0;
+    }
+    if (n == 2){
         return 1;
     }
     return f(n - 1) + f(n - 2);
